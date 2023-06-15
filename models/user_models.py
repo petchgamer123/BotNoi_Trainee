c02ff8c8e8c28f378eb8a611e83fd1364033453f
petchgamer123 <69622369+petchgamer123@users.noreply.github.com>
2023-06-15 13:28:53 +0700

    Update user_models.py

--- models/user_models.py
+++ models/user_models.py
@@ -1,5 +1,12 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel
 
 class User(BaseModel):
     user_id: str
     name: str
+    age: str
+    gender: str
+
+class data(BaseModel):
+    age: str
+    gender: str
+
