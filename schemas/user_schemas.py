e8127e0ef26c40db870c03a9b019904ced1bd7aa
petchgamer123 <69622369+petchgamer123@users.noreply.github.com>
2023-06-15 13:28:32 +0700

    Update user_schemas.py

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -2,7 +2,9 @@ def user_serializer(user) -> dict:
     return {
         "id": str(user["_id"]),
         "user_id": user["user_id"],
-        "name": user["name"]
+        "name": user["name"],
+        "age": user["age"],
+        "gender": user["gender"]
     }
 
 def users_serializer(users) -> list:
